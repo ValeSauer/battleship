a82b36af3f327d180d480e7a33ad85733492607c
Valentin Sauer <valentin@sauer-medientechnik.de>
2016-05-06 23:04:40 +0200

    added clipping ;-)

--- app/pages/game-setup/game-setup.ts
+++ app/pages/game-setup/game-setup.ts
@@ -47,6 +47,7 @@ export class GameSetup {
         ship.setAttribute('data-type', type.toString());
         ship.setAttribute('data-x', '0');
         ship.setAttribute('data-y', this.shipCount.toString());
+        ship.setAttribute('data-orientation', 'horizontal');
 
         ship.className = 'ship ship-' + type;
         ship.style.height = this.cellSize + 'px';
@@ -63,14 +64,49 @@ export class GameSetup {
         ship.addEventListener('touchend', () => {
             if (ship.getAttribute('data-moved') === 'false') {
                 ship.classList.toggle('rotate');
+                if(ship.getAttribute('data-orientation') == 'horizontal'){
+                    ship.setAttribute('data-orientation', 'vertical');
+                }else{
+                    ship.setAttribute('data-orientation', 'horizontal');
+                }
             }
 
-            ship.setAttribute('data-x', Math.round(parseFloat(ship.style.left) / this.cellSize).toString());
-            ship.setAttribute('data-y', Math.round(parseFloat(ship.style.top) / this.cellSize).toString());
+            var newX = Math.floor(parseFloat(ship.style.left) / this.cellSize);
+            var newY = Math.floor(parseFloat(ship.style.top) / this.cellSize);
+
+            if(this.moveAllowed(newX, newY, ship)){
+                ship.setAttribute('data-x', newX.toString());
+                ship.setAttribute('data-y', newY.toString());
+            }else{
+                ship.style.left = (this.cellSize * ship.getAttribute('data-x')) + 'px';
+                ship.style.top = (this.cellSize * ship.getAttribute('data-y')) + 'px';
+                return;
+            }
+            console.log("X: " + ship.getAttribute('data-x') + " Y: " + ship.getAttribute('data-y'));
         })
 
         game.appendChild(ship);
         this.shipCount++;
+    }
+
+    moveAllowed(newX:number, newY: number, ship:HTMLElement): boolean{
+        for(let i = 0; i < ship.getAttribute('data-type'); i++){
+            if(newX > 9 || newX < 0 || newY > 9 || newY < 0){
+                return false;
+            }
+            for(var checkship of document.getElementsByClassName('ship')){
+                if(newX == checkship.getAttribute('data-x') && newY == checkship.getAttribute('data-y') && ship != checkship){
+                    return false;
+                }
+            }
+
+            if(ship.getAttribute('data-orientation') == 'horizontal'){
+                newX++;
+            }else{
+                newY++
+            }
+        }
+        return true;
     }
 
     ngAfterContentInit() {
@@ -95,8 +131,8 @@ export class GameSetup {
         interact('.ship')
             .draggable({
                 relativePoints: [
-                    { x: 0  , y: 0   }   // snap relative to the element's top-left,
-                ]
+                    { x: 0  , y: 0 }   // snap relative to the element's top-left,
+                ],
                 restrict: {
                     restriction: 'parent',
                     elementRect: {top: 0, left: 0, bottom: 1, right: 1},
@@ -115,6 +151,10 @@ export class GameSetup {
                     event.target.setAttribute('data-moved', 'true');
                     event.target.style.top = (event.pageY - this.originY) + 'px';
                     event.target.style.left = (event.pageX - this.originX) + 'px';
+                },
+                restrict: {
+                    drag: "parent",
+                    elementRect: {top: 0, left: 0, bottom: 1, right: 1}
                 }
             });
     }
