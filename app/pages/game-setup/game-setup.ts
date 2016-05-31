e3b03fd4cc0de0c9ebe0cb4c48d12db5e8c6b301
Valentin Sauer <valentin@sauer-medientechnik.de>
2016-05-31 18:39:41 +0200

    trying to use angular

--- app/pages/game-setup/game-setup.ts
+++ app/pages/game-setup/game-setup.ts
@@ -1,37 +1,45 @@
 import {Page, NavController} from 'ionic-angular';
 import {GamePlay} from '../game-play/game-play';
+import {Player} from '../game-setup/player.ts';
+import {Ship} from '../game-setup/ship.ts';
 import * as interact from 'interact.js';
+import {IPlayer} from "./IPlayer";
 
 
 @Page({
-    templateUrl: 'build/pages/game-setup/game-setup.html',
+    templateUrl: 'build/pages/game-setup/game-setup.html'
 })
 export class GameSetup {
 
     cells:Array<number> = [];
-    originX:number;
-    originY:number;
-    cellSize:number;
-    shipCount:number = 0;
+    x:number = 10;
+    y:number = 10;
+
+    players:Array<IPlayer> = [new Player(1, false), new Player(2, true)];
+
+    static originX:number;
+    static originY:number;
+    static cellSize:number;
+    currentPlayer:Player = this.players[0];
 
     constructor(private nav:NavController) {
-        for (let i = 0; i < 100; i++) {
+        for (let i = 0; i < this.x * this.y; i++) {
             this.cells.push(i);
         }
     }
 
-    startGame(){
+    startGame() {
 
         let shipElements = document.getElementsByClassName('ship');
         let ships = [];
 
-        for (let i = 0; i < shipElements.length; i++){
+        for (let i = 0; i < shipElements.length; i++) {
             let shipElement = shipElements.item(i);
             let ship = {
-                orientation : shipElement.classList.contains('rotate') ? 'vertical' : 'horizontal',
-                type : shipElement.getAttribute('data-type'),
-                x : parseInt(shipElement.getAttribute('data-x')),
-                y : parseInt(shipElement.getAttribute('data-y'))
+                x: parseInt(shipElement.getAttribute('data-x')),
+                y: parseInt(shipElement.getAttribute('data-y')),
+                type: shipElement.getAttribute('data-type'),
+                orientation: shipElement.classList.contains('rotate') ? 'vertical' : 'horizontal'
             }
 
             ships.push(ship);
@@ -42,67 +50,20 @@ export class GameSetup {
         });
     }
 
-    initShip(game:HTMLElement, type:number) {
-        let ship = document.createElement('div');
-        ship.setAttribute('data-type', type.toString());
-        ship.setAttribute('data-x', '0');
-        ship.setAttribute('data-y', this.shipCount.toString());
-        ship.setAttribute('data-orientation', 'horizontal');
-
-        ship.className = 'ship ship-' + type;
-        ship.style.height = this.cellSize + 'px';
-        ship.style.width = (this.cellSize * type) + 'px';
-        ship.style.left = '0px';
-        ship.style.top = (this.cellSize * this.shipCount) + 'px';
-
-        ship.style.transformOrigin = this.cellSize / 2 + 'px ' + this.cellSize / 2 + 'px';
-
-        ship.addEventListener('touchstart', () => {
-            ship.setAttribute('data-moved', 'false');
-        })
-
-        ship.addEventListener('touchend', () => {
-            if (ship.getAttribute('data-moved') === 'false') {
-                ship.classList.toggle('rotate');
-                if(ship.getAttribute('data-orientation') == 'horizontal'){
-                    ship.setAttribute('data-orientation', 'vertical');
-                }else{
-                    ship.setAttribute('data-orientation', 'horizontal');
-                }
-            }
-
-            var newX = Math.floor(parseFloat(ship.style.left) / this.cellSize);
-            var newY = Math.floor(parseFloat(ship.style.top) / this.cellSize);
-
-            if(this.moveAllowed(newX, newY, ship)){
-                ship.setAttribute('data-x', newX.toString());
-                ship.setAttribute('data-y', newY.toString());
-            }else{
-                ship.style.left = (this.cellSize * ship.getAttribute('data-x')) + 'px';
-                ship.style.top = (this.cellSize * ship.getAttribute('data-y')) + 'px';
-                return;
-            }
-            console.log("X: " + ship.getAttribute('data-x') + " Y: " + ship.getAttribute('data-y'));
-        })
-
-        game.appendChild(ship);
-        this.shipCount++;
-    }
-
-    moveAllowed(newX:number, newY: number, ship:HTMLElement): boolean{
-        for(let i = 0; i < ship.getAttribute('data-type'); i++){
-            if(newX > 9 || newX < 0 || newY > 9 || newY < 0){
+    moveAllowed(newX:number, newY:number, ship:HTMLElement):boolean {
+        for (let i = 0; i < ship.getAttribute('data-type'); i++) {
+            if (newX > 9 || newX < 0 || newY > 9 || newY < 0) {
                 return false;
             }
-            for(var checkship of document.getElementsByClassName('ship')){
-                if(newX == checkship.getAttribute('data-x') && newY == checkship.getAttribute('data-y') && ship != checkship){
+            for (var checkship of document.getElementsByClassName('ship')) {
+                if (newX == checkship.getAttribute('data-x') && newY == checkship.getAttribute('data-y') && ship != checkship) {
                     return false;
                 }
             }
 
-            if(ship.getAttribute('data-orientation') == 'horizontal'){
+            if (ship.getAttribute('data-orientation') == 'horizontal') {
                 newX++;
-            }else{
+            } else {
                 newY++
             }
         }
@@ -114,24 +75,18 @@ export class GameSetup {
         let game = document.getElementById('game');
         game.style.height = game.clientWidth + 'px';
 
-        this.cellSize = game.clientWidth / 10;
-        this.originX = game.getClientRects()[0].left;
-        this.originY = game.getClientRects()[0].top;
+        GameSetup.cellSize = game.clientWidth / 10;
+        GameSetup.originX = game.getClientRects()[0].left;
+        GameSetup.originY = game.getClientRects()[0].top;
 
-        let z = 4;
-        for (let i = 2; i <= 5; i++) {
-            let j = 0;
-            while (j < z) {
-                this.initShip(game, i);
-                j++;
-            }
-            z--;
+        for (var ship of this.currentPlayer.ships){
+            //ship.render();
         }
 
         interact('.ship')
             .draggable({
                 relativePoints: [
-                    { x: 0  , y: 0 }   // snap relative to the element's top-left,
+                    {x: 0, y: 0}   // snap relative to the element's top-left,
                 ],
                 restrict: {
                     restriction: 'parent',
@@ -141,16 +96,16 @@ export class GameSetup {
                 snap: {
                     targets: [
                         interact.createSnapGrid({
-                            x: this.cellSize,
-                            y: this.cellSize,
-                            offset: {x: this.originX, y: this.originY}
+                            x: GameSetup.cellSize,
+                            y: GameSetup.cellSize,
+                            offset: {x: GameSetup.originX, y: GameSetup.originY}
                         })
                     ],
                 },
                 onmove: (event) => {
                     event.target.setAttribute('data-moved', 'true');
-                    event.target.style.top = (event.pageY - this.originY) + 'px';
-                    event.target.style.left = (event.pageX - this.originX) + 'px';
+                    event.target.style.top = (event.pageY - GameSetup.originY) + 'px';
+                    event.target.style.left = (event.pageX - GameSetup.originX) + 'px';
                 },
                 restrict: {
                     drag: "parent",
