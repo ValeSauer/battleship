04c3ad5d7dd940db1f063494a14111c86157c36b
Valentin Sauer <valentin@sauer-medientechnik.de>
2016-06-04 10:49:26 +0200

    trying to use angular

---
+++ app/pages/game-setup/aiplayer.ts
@@ -0,0 +1,28 @@
+/**
+ * Created by valentin.sauer on 30.05.2016.
+ */
+import {Ship} from '../game-setup/ship.ts';
+import {IPlayer} from '../game-setup/IPlayer.ts';
+
+export class AIPlayer implements IPlayer {
+    id:number;
+    ships:Array<Ship> = [];
+
+    constructor(id:number) {
+        this.id = id;
+
+        for (let i = 0; i < 10; i++) {
+            this.ships.push(new Ship(0, i, 2));
+            this.ships.push(new Ship(0, i, 2));
+            this.ships.push(new Ship(0, i, 2));
+            this.ships.push(new Ship(0, i, 2));
+            this.ships.push(new Ship(0, i, 3));
+            this.ships.push(new Ship(0, i, 3));
+            this.ships.push(new Ship(0, i, 3));
+            this.ships.push(new Ship(0, i, 4));
+            this.ships.push(new Ship(0, i, 4));
+            this.ships.push(new Ship(0, i, 5));
+        }
+
+    }
+}
